5335f95387da6f014518a4274f8cee9e19b40e9b
Jesse Ireland <j_ireland@mac.com>
2024-12-01 10:05:06 -0500

    Completed second task of day 1

---
+++ 01-part2.rs
@@ -0,0 +1,83 @@
+/*
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+For these example lists, here is the process of finding the similarity score:
+
+The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+The fourth number, 1, also does not appear in the right list.
+The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+*/
+
+use std::collections::HashMap;
+use std::fs;
+
+fn main()
+{
+    let file_path = "/Users/jesseireland/Documents/advent-of-code/advent-of-code-2024/01.in".to_string();
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let mut left_values = Vec::new();
+    let mut occurences_in_right_list = HashMap::new();
+
+    for line in contents.lines()
+    {
+        let mut parts = line.split_whitespace().map(|s| s.parse::<i64>());
+        match (parts.next(), parts.next())
+        {
+            (Some(Ok(left)), Some(Ok(right))) =>
+            {
+                left_values.push(left);
+
+                let contains_key = occurences_in_right_list.contains_key(&right);
+                if contains_key
+                {
+                    let current_count = occurences_in_right_list.get(&right).unwrap();
+                    occurences_in_right_list.insert(right, current_count + 1);
+                }
+                else
+                {
+                    occurences_in_right_list.insert(right, 1);
+                }
+            }
+            _ => {}  // ignore invalid input
+        }
+    }
+
+    let mut similarity_score = 0;
+
+    for value in left_values.iter()
+    {
+        let contains_key = occurences_in_right_list.contains_key(&value);
+        if contains_key
+        {
+            let count = occurences_in_right_list.get(&value).unwrap();
+            let similarity = value * count;
+            similarity_score += similarity;
+        }
+    }
+    println!("{}", similarity_score);
+
+    // Guess 1: 26674158 (correct)
+}
